d60f9991e89700ebe53ccb30b96c5e76bf8b01d7
EfrosEugeniu-md <efrosevgenij@gmail.com>
2024-03-05 10:24:21 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from 'react-router-dom';
+import { PageOne, PageTwo } from './Components/Pages';
+import {MainPage} from './Components/MainPage';
+
+const App = () => {
+
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<MainPage />} >
+          <Route index element={<div>No page is selected.</div> } />
+          <Route path="one" element={<PageOne />} />
+          <Route path="two" element={<PageTwo />} />
+        </Route>
+    </Routes>
+  </BrowserRouter>
+  );
+}
+
+export default App;
